8af38295c102ec99dc1a73db43bc3e996ce780b6
Ryan Ferreira <heyryanf@hotmail.com>
2026-06-05 19:41:22 -0300

    feat: replace svg logo with official png asset

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,18 +2,11 @@ import React from 'react'
 
 export function Logo({ className = "h-8 w-auto" }: { className?: string }) {
   return (
-    <svg 
-      viewBox="0 14 120 66" 
-      fill="currentColor" 
+    <img 
+      src="/logo.png" 
+      alt="Memvo Logo" 
       className={className} 
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d="M 0 50 
-               A 25 25 0 0 1 50 50 
-               A 35 35 0 0 1 120 50 
-               L 120 80 
-               L 0 80 
-               Z" />
-    </svg>
+      style={{ objectFit: 'contain' }}
+    />
   )
 }
