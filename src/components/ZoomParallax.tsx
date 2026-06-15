46e65f1920677310e1b19f27dcfd4182ef8d5b4a
Ryan Ferreira <heyryanf@hotmail.com>
2026-06-15 16:52:04 -0300

    fix: ZoomParallax com next/image otimizado (correção candidata ao travamento)

--- src/components/ZoomParallax.tsx
+++ src/components/ZoomParallax.tsx
@@ -3,6 +3,7 @@
 import { useScroll, useTransform, motion } from 'framer-motion';
 import { useRef } from 'react';
 import { Camera } from 'lucide-react';
+import NextImage from 'next/image';
 
 interface Image {
 	src: string;
@@ -47,9 +48,12 @@ export function ZoomParallax({ images }: ZoomParallaxProps) {
 							className={`absolute top-0 flex h-full w-full items-center justify-center ${index === 1 ? '[&>div]:!-top-[30vh] [&>div]:!left-[5vw] [&>div]:!h-[30vh] [&>div]:!w-[35vw]' : ''} ${index === 2 ? '[&>div]:!-top-[10vh] [&>div]:!-left-[25vw] [&>div]:!h-[45vh] [&>div]:!w-[20vw]' : ''} ${index === 3 ? '[&>div]:!left-[27.5vw] [&>div]:!h-[25vh] [&>div]:!w-[25vw]' : ''} ${index === 4 ? '[&>div]:!top-[27.5vh] [&>div]:!left-[5vw] [&>div]:!h-[25vh] [&>div]:!w-[20vw]' : ''} ${index === 5 ? '[&>div]:!top-[27.5vh] [&>div]:!-left-[22.5vw] [&>div]:!h-[25vh] [&>div]:!w-[30vw]' : ''} ${index === 6 ? '[&>div]:!top-[22.5vh] [&>div]:!left-[25vw] [&>div]:!h-[15vh] [&>div]:!w-[15vw]' : ''} `}
 						>
 							<div className="relative h-[25vh] w-[25vw]">
-								<img
+								<NextImage
 									src={src || '/placeholder.svg'}
 									alt={alt || `Parallax image ${index + 1}`}
+									width={480}
+									height={270}
+									quality={75}
 									className="h-full w-full object-cover rounded-lg shadow-lg"
 								/>
 
@@ -84,9 +88,12 @@ export function ZoomParallax({ images }: ZoomParallaxProps) {
 						key={`mobile-${index}`}
 						className={`relative w-full overflow-hidden rounded-2xl shadow-sm ${index === 0 ? 'col-span-2 aspect-[2/1]' : 'aspect-square'}`}
 					>
-						<img
+						<NextImage
 							src={src || '/placeholder.svg'}
 							alt={alt || `Mobile image ${index + 1}`}
+							width={480}
+							height={270}
+							quality={75}
 							className="h-full w-full object-cover hover:scale-105 transition-transform duration-500"
 						/>
 						{challenge && (
